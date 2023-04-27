d9e9bb67cd384cb8cc6685c56c915cfd18b9225d
oricardos <ricardo.rs95@gmail.com>
2023-04-26 23:11:45 -0300

    feat(forms): adicionando novos campos

---
+++ tickets/class_type.py
@@ -0,0 +1,5 @@
+class_types = {
+    (1, 'Econômica'),
+    (2, 'Executiva'),
+    (3, 'Primeira Classe')
+}
--- tickets/forms.py
+++ tickets/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from tickets.class_type import class_types
 
 class TicketsForm(forms.Form):
     origin = forms.CharField(label="Origem", max_length=100)
     destiny = forms.CharField(label="Destino", max_length=100)
     departure_date = forms.DateField(label="Data de partida", widget=DatePicker())
     back_date = forms.DateField(label="Data de volta", widget=DatePicker())
+    search_date = forms.DateField(label="Data da pesquisa", disabled=True, initial=datetime.today)
+    class_type = forms.ChoiceField(label="TIpo de classe", choices=class_types)
+    infos = forms.CharField(
+        label="Informações extras",
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label="Email", max_length=200)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
